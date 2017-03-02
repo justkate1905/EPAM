3979e8e87f83723fbdea36092d6829f821e39944
Ekaterina <kate.son2015@yandex.ru>
2017-03-02 14:05:57 +0300

    Changed logic of storing marks. Green test for adding wrong types of marks for disciplines

--- unit02/task05/src/main/java/epam/Student.java
+++ unit02/task05/src/main/java/epam/Student.java
@@ -1,36 +1,40 @@
 package epam;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Katerina on 01.03.2017.
  */
 public class Student {
     private String name;
-    private Map<Disciplines, Mark<? extends Number>> marks;
+    private Map<Disciplines, List<Mark<? extends Number>>> marks;
 
     public Student(){
         name = "John";
-        marks = new HashMap<Disciplines, Mark<? extends Number>>();
+        marks = new HashMap<Disciplines, List<Mark<? extends Number>>>();
     }
     public Student(String _name){
         name = _name;
-        marks = new HashMap<Disciplines, Mark<? extends Number>>();
+        marks = new HashMap<Disciplines, List<Mark<? extends Number>>>();
     }
 
     public void addDisciplineWithMark(Disciplines d, Mark<? extends Number> m) {
         if(d.getType()!=m.getMark().getClass()) {
             throw new IllegalArgumentException("Type of mark doesn't match to the discipline");
         }
+        else if(marks.containsKey(d)){
+            marks.get(d).add(m);
+        }
         else{
-            marks.put(d,m);
+            List<Mark<? extends Number>> newMarks = new ArrayList<Mark<? extends Number>>();
+            newMarks.add(m);
+            marks.put(d, newMarks);
         }
     }
 
-    public Map<Disciplines, Mark<? extends Number>> getMarks(){
-        return new HashMap<Disciplines, Mark<? extends Number>>(marks);
+
+    public Map<Disciplines,List<Mark<? extends Number>>> getMarks(){
+        return new HashMap<Disciplines, List<Mark<? extends Number>>>(marks);
     }
 
     public String getName(){
--- unit02/task05/src/test/java/epam/GroupsTest.java
+++ unit02/task05/src/test/java/epam/GroupsTest.java
@@ -3,15 +3,27 @@ package epam;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
 
 class GroupsTest extends TestCase {
+
+
+    Student John = new Student("John");
     @Test
     public void testCreatingStudentsAndAddingRightTypeOfMarks() {
-        Student John = new Student("John");
         John.addDisciplineWithMark(Disciplines.ENGLISH, new IntegerMark(5));
         John.addDisciplineWithMark(Disciplines.MATHEMATICS, new DoubleMark(2.5));
-        John.addDisciplineWithMark(Disciplines.HISTORY, new IntegerMark(7));
         assertTrue(John.getMarks().containsKey(Disciplines.ENGLISH));
+        assertTrue(John.getMarks().containsKey(Disciplines.MATHEMATICS));
+
+    }
+    @Test
+    public void testAddingWrongTypeOfMarksWithException() {
+        DoubleMark d = new DoubleMark(4.5);
+        try {
+            John.addDisciplineWithMark(Disciplines.ENGLISH,d);
+        }
+        catch (IllegalArgumentException e){
+            assertTrue(e.getMessage().equals("Type of mark doesn't match to the discipline"));
+        }
     }
 }
