bcb09d5871d25aafd093e4f38733485df7e6d695
Ekaterina <kate.son2015@yandex.ru>
2017-03-20 00:10:12 +0300

    Refactor task01. Using only byte streams

--- unit04/src/main/java/t01/KeywordsParser.java
+++ unit04/src/main/java/t01/KeywordsParser.java
@@ -12,73 +12,72 @@ public class KeywordsParser {
     public static String outputFile;
 
     private HashMap<String, Integer> keyWords = new HashMap<String, Integer>();
-    public KeywordsParser(){
+
+    public KeywordsParser() {
 
     }
-    public KeywordsParser(String keyWords, String input, String output){
+
+    public KeywordsParser(String keyWords, String input, String output) {
         keywordsFile = keyWords;
         inputFile = input;
         outputFile = output;
     }
 
-    public static KeywordsParser build(String keyWords, String input, String output){
+    public static KeywordsParser build(String keyWords, String input, String output) {
         return new KeywordsParser(keyWords, input, output);
     }
 
     private void makeKeyWordsMap(String keywordsFile) throws IOException {
+
         FileInputStream fileInputStream = new FileInputStream(keywordsFile);
 
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+        int bytesAvailable = fileInputStream.available();
+        byte[] bytesReaded = new byte[bytesAvailable];
 
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        fileInputStream.read(bytesReaded, 0, bytesAvailable);
+        String words = new String(bytesReaded);
 
-        String line;
+        fileInputStream.close();
 
-        while((line = bufferedReader.readLine())!=null){
-            for (String s : line.split(" ")) {
-                keyWords.put(s,0);
+            for (String s : words.split(" ")) {
+                keyWords.put(s, 0);
             }
-        }
-        bufferedReader.close();
 
     }
 
-    public void makeFileWithKeyWordsCount(){
-        try{
+    public void makeFileWithKeyWordsCount() {
+        try {
             makeKeyWordsMap(keywordsFile);
             readCodeFromFile(inputFile);
             writeGystogramToFile(outputFile);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private void readCodeFromFile(String inputFile) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(inputFile);
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String line;
 
-        while((line = bufferedReader.readLine())!=null){
-            for (String s : line.split(" ")) {
-                if(keyWords.containsKey(s)){
-                    keyWords.put(s, keyWords.get(s)+1);
+        int bytesAvailable = fileInputStream.available();
+
+        byte[] bytesReaded = new byte[bytesAvailable];
+
+        fileInputStream.read(bytesReaded, 0, bytesAvailable);
+        fileInputStream.close();
+        String code = new String(bytesReaded);
+
+        for (String s : code.split(" ")) {
+                if (keyWords.containsKey(s)) {
+                    keyWords.put(s, keyWords.get(s) + 1);
                 }
             }
         }
-    }
 
     private void writeGystogramToFile(String outputFile) throws IOException {
         FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+        fileOutputStream.write(getKeyWords().toString().getBytes());
 
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
-
-        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-        bufferedWriter.write(getKeyWords().toString());
-
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        fileOutputStream.close();
     }
 
 
