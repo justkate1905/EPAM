f84d317d1db82f999e759420f2868362b5705430
Ekaterina <kate.son2015@yandex.ru>
2017-04-01 12:34:44 +0300

    Methods for write and read of files

--- unit05/src/main/java/t01/FileManager.java
+++ unit05/src/main/java/t01/FileManager.java
@@ -1,11 +1,10 @@
 package t01;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,6 +52,14 @@ public class FileManager {
         if (file.exists()) {
             if (file.isDirectory()) {
                 currentPath = temp;
+            } else {
+                System.out.println("File is not directory");
+            }
+        } else {
+            try {
+                throw new NoSuchFileException(temp);
+            } catch (NoSuchFileException e) {
+                System.out.println("No such directory");
             }
         }
     }
@@ -64,7 +71,6 @@ public class FileManager {
     }
 
     public int create(String name, String type) {
-
         if (!type.equals("txt")) {
             System.out.println("You can create only text files");
             return -1;
@@ -92,8 +98,6 @@ public class FileManager {
 
     public int delete(String nameOfFile) {
 
-
-
         final String pathname = currentPath + "\\" + nameOfFile;
         File file = new File(pathname);
 
@@ -112,6 +116,65 @@ public class FileManager {
                 System.out.println(e.getMessage());
                 return 1;
             }
+        }
+    }
+
+    public int write(String fileName, String text, boolean flag) {
+        String path;
+        if (fileName.contains("\\")) {
+            path = fileName;
+        } else {
+            path = currentPath + "\\" + fileName;
+        }
+        try {
+            File file = new File(path);
+            if (file.exists()) {
+                if (!path.endsWith(".txt")) {
+                    return 1;
+                }
+                FileWriter fileWriter = new FileWriter(file, flag);
+                fileWriter.write(text);
+                fileWriter.flush();
+                fileWriter.close();
+                return 0;
+            } else {
+                System.out.println("No such file");
+                return 1;
+            }
+        } catch (IOException e) {
+            return -1;
+        }
+    }
+
+    public int read(String fileName) {
+        String path;
+        StringBuilder text = new StringBuilder();
+        if (fileName.contains("\\")) {
+            path = fileName;
+        } else {
+            path = currentPath + "\\" + fileName;
+        }
+        try {
+            File file = new File(path);
+            if (file.exists()) {
+                if (!path.endsWith(".txt")) {
+                    System.out.println("You can read only text files!");
+                    return 1;
+                }
+                FileReader fileReader = new FileReader(file);
+                int c;
+                while ((c = fileReader.read()) != -1) {
+                    text.append((char)c);
+                }
+                fileReader.close();
+                System.out.println(text.toString());
+                return 0;
+            } else {
+                System.out.println("No such file");
+                return 1;
+            }
+        } catch (IOException e) {
+            return -1;
         }
     }
 }
--- unit05/src/test/java/t01/FileManagerTest.java
+++ unit05/src/test/java/t01/FileManagerTest.java
@@ -16,44 +16,57 @@ import static t01.FileManager.FILES;
  */
 public class FileManagerTest {
 
-    private FileManager fileManager  =new FileManager();
+    private FileManager fileManager = new FileManager();
 
     @Test
-    public void testThatUserCanSeeListOfOnlyDirs(){
+    public void testThatUserCanSeeListOfOnlyDirs() {
         FileManager fileManager = new FileManager();
-        List<String> dirsList = fileManager.getRequiredList("./",DIRS);
+        List<String> dirsList = fileManager.getRequiredList("./", DIRS);
         assertThat(dirsList.contains("src"), is(true));
     }
 
     @Test
-    public void testThatUserCanSeeListOfOnlyFiles(){
+    public void testThatUserCanSeeListOfOnlyFiles() {
         List<String> filesList = fileManager.getRequiredList("./", FILES);
         assertThat(filesList.contains("pom.xml"), is(true));
     }
 
     @Test
-    public void testThatWeCanGoToTheDirectory(){
+    public void testThatWeCanGoToTheDirectory() {
         fileManager.goToDirectory("src");
         assertTrue(fileManager.currentPath.contains("src"));
     }
 
     @Test
-    public void testThatWeCanGoToTheParentDirectory(){
+    public void testThatWeCantTakeFileAsDirectory() {
+        fileManager.goToDirectory("src");
+        fileManager.goToDirectory("file.d");
+        assertFalse(fileManager.currentPath.contains("file.d"));
+    }
+
+    @Test
+    public void testThatWeCantGoToNonExistingDirectory() {
+        fileManager.goToDirectory("java");
+        assertFalse(fileManager.currentPath.contains("java"));
+    }
+
+    @Test
+    public void testThatWeCanGoToTheParentDirectory() {
         fileManager.goToDirectory("src");
         fileManager.goUp();
         assertFalse(fileManager.currentPath.contains("src"));
     }
 
     @Test
-    public void testThatWeCanCreateTextFile(){
+    public void testThatWeCanCreateTextFile() {
         fileManager.goToDirectory("src");
         fileManager.goToDirectory("resource");
         fileManager.create("newFile", "txt");
-        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.txt"));
+        assertFalse(fileManager.getRequiredList("./", ALL).contains("newFile.txt"));
     }
 
     @Test
-    public void testThatWeCantCreateExistingFile(){
+    public void testThatWeCantCreateExistingFile() {
         fileManager.goToDirectory("src");
         fileManager.goToDirectory("resource");
         int result = fileManager.create("newFile", "txt");
@@ -61,25 +74,77 @@ public class FileManagerTest {
     }
 
     @Test
-    public void testThatWeCantCreateAnotherFile(){
+    public void testThatWeCantCreateAnotherFile() {
         fileManager.goToDirectory("src");
         fileManager.create("newFile", "tsx");
-        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.tsx"));
+        assertFalse(fileManager.getRequiredList("./", ALL).contains("newFile.tsx"));
     }
 
     @Test
-    public void testThatWeCanDeleteTextFile(){
+    public void testThatWeCanDeleteTextFile() {
         fileManager.goToDirectory("src");
         fileManager.goToDirectory("resource");
         fileManager.delete("newFile.txt");
-        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.txt"));
+        assertFalse(fileManager.getRequiredList("./", ALL).contains("newFile.txt"));
     }
 
     @Test
-    public void testThatWeCantDeleteNonExistingFile(){
+    public void testThatWeCantDeleteNonExistingFile() {
         fileManager.goToDirectory("src");
         fileManager.delete("newFile.tsx");
-        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.tsx"));
+        assertFalse(fileManager.getRequiredList("./", ALL).contains("newFile.tsx"));
+    }
+
+    @Test
+    public void testThatWeCantDeleteNonTextFile() {
+        fileManager.goToDirectory("src");
+        fileManager.delete("file.d");
+        assertFalse(fileManager.getRequiredList("./", ALL).contains("newFile.tsx"));
+    }
+
+    @Test
+    public void testThatWeCanModifyTextFiles() {
+        String myText = "Hello! This is text for file modifying.";
+        fileManager.goToDirectory("src");
+        int result = fileManager.write("newFile.txt", myText,true);
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void testThatWeCantModifyNonTextFiles() {
+        String myText = "Hello! This is text for file modifying";
+        fileManager.goToDirectory("src");
+        int result = fileManager.write("file.d", myText,true);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void testThatWeCantModifyNonExistingFiles() {
+        String myText = "Hello! This is text for file modifying";
+        fileManager.goToDirectory("src");
+        int result = fileManager.write("file.txt", myText,true);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void testThatWeCanReadTextFiles() {
+        fileManager.goToDirectory("src");
+        int result = fileManager.read("newFile.txt");
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void testThatWeCantReadNonTextFiles() {
+        fileManager.goToDirectory("src");
+        int result = fileManager.read("file.d");
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void testThatWeCantReadNonExistingFiles() {
+        fileManager.goToDirectory("src");
+        int result = fileManager.read("file.txt");
+        assertThat(result, is(1));
     }
 
 
