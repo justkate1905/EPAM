9e891f79d938db9f0d95b24dcfd4fe82cfa608fc
Ekaterina <kate.son2015@yandex.ru>
2017-04-01 10:17:31 +0300

    deleting and creating methods

--- unit05/src/main/java/t01/FileManager.java
+++ unit05/src/main/java/t01/FileManager.java
@@ -1,6 +1,8 @@
 package t01;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,13 +13,15 @@ import java.util.List;
 public class FileManager {
 
     public static final String FILES = "f";
-    public static final  String DIRS = "d";
+    public static final String DIRS = "d";
+    public static final String ALL = "all";
 
-    public String currentPath = Paths.get("").toAbsolutePath().toString();
 
-    public List<String> getRequiredList(String s, String type) {
+    public String currentPath = System.getProperty("user.dir");
+
+    public List<String> getRequiredList(String path, String type) {
         List<String> requiredList = new ArrayList<String>();
-        File dir = new File(s);
+        File dir = new File(path);
         for (File entry : dir.listFiles()) {
             switch (type) {
                 case FILES:
@@ -30,23 +34,84 @@ public class FileManager {
                         requiredList.add(entry.getName());
                     }
                     break;
+                case ALL:
+                    requiredList.add(entry.getName());
+                    break;
             }
         }
-
         return requiredList;
     }
 
     public void goToDirectory(String src) {
-        File file = new File(currentPath+"\\"+src);
-        if(file.exists()){
-            if(file.isDirectory()){
-                currentPath = currentPath+"\\"+src;
+        String temp;
+        if (src.contains("\\")) {
+            temp = src;
+        } else {
+            temp = currentPath + "\\" + src;
+        }
+        File file = new File(temp);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                currentPath = temp;
             }
         }
     }
 
-    public void goUp(){
-        int lastIndex = currentPath.lastIndexOf("\\");
-        currentPath = currentPath.substring(0,lastIndex);
+
+    public void goUp() {
+        File file = new File(currentPath);
+        currentPath = file.getParentFile().toString();
+    }
+
+    public int create(String name, String type) {
+
+        if (!type.equals("txt")) {
+            System.out.println("You can create only text files");
+            return -1;
+        }
+        final String pathname = currentPath + "\\" + name + "." + type;
+        File file = new File(pathname);
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                return 0;
+
+            } catch (IOException e) {
+                return 1;
+            }
+
+        } else {
+            try {
+                throw new FileAlreadyExistsException(pathname);
+            } catch (FileAlreadyExistsException e) {
+                return 1;
+            }
+        }
+    }
+
+    public int delete(String nameOfFile) {
+
+
+
+        final String pathname = currentPath + "\\" + nameOfFile;
+        File file = new File(pathname);
+
+        if (file.exists()) {
+            if (!nameOfFile.endsWith(".txt")) {
+                System.out.println("You can delete only text files");
+                return -1;
+            }
+            file.delete();
+            return 0;
+
+        } else {
+            try {
+                throw new IOException("No such file");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                return 1;
+            }
+        }
     }
 }
--- unit05/src/test/java/t01/FileManagerTest.java
+++ unit05/src/test/java/t01/FileManagerTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static t01.FileManager.ALL;
 import static t01.FileManager.DIRS;
 import static t01.FileManager.FILES;
 
@@ -42,5 +43,44 @@ public class FileManagerTest {
         fileManager.goUp();
         assertFalse(fileManager.currentPath.contains("src"));
     }
+
+    @Test
+    public void testThatWeCanCreateTextFile(){
+        fileManager.goToDirectory("src");
+        fileManager.goToDirectory("resource");
+        fileManager.create("newFile", "txt");
+        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.txt"));
+    }
+
+    @Test
+    public void testThatWeCantCreateExistingFile(){
+        fileManager.goToDirectory("src");
+        fileManager.goToDirectory("resource");
+        int result = fileManager.create("newFile", "txt");
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void testThatWeCantCreateAnotherFile(){
+        fileManager.goToDirectory("src");
+        fileManager.create("newFile", "tsx");
+        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.tsx"));
+    }
+
+    @Test
+    public void testThatWeCanDeleteTextFile(){
+        fileManager.goToDirectory("src");
+        fileManager.goToDirectory("resource");
+        fileManager.delete("newFile.txt");
+        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.txt"));
+    }
+
+    @Test
+    public void testThatWeCantDeleteNonExistingFile(){
+        fileManager.goToDirectory("src");
+        fileManager.delete("newFile.tsx");
+        assertFalse(fileManager.getRequiredList("./",ALL).contains("newFile.tsx"));
+    }
+
 
 }
