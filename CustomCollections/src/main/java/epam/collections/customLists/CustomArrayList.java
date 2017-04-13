454fbb69a53ed3338cade4793f5827c8808c7e02
Ekaterina <kate.son2015@yandex.ru>
2017-04-13 15:25:22 +0300

    ListIterator for CustomArrayList

--- CustomCollections/src/main/java/epam/collections/customLists/CustomArrayList.java
+++ CustomCollections/src/main/java/epam/collections/customLists/CustomArrayList.java
@@ -35,7 +35,22 @@ public class CustomArrayList<T> implements List<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new Iterator<T>() {
+            private int current = 0;
+
+            @Override
+            public boolean hasNext() {
+                return current < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) data[current];
+            }
+        };
     }
 
     @Override
@@ -46,14 +61,14 @@ public class CustomArrayList<T> implements List<T> {
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        if (a.length < size){
+        if (a.length < size) {
             throw new IndexOutOfBoundsException();
         }
         Object[] array = a;
         for (int i = 0; i < size; i++) {
             array[i] = data[i];
         }
-        return (T1[])array;
+        return (T1[]) array;
     }
 
     @Override
@@ -75,6 +90,50 @@ public class CustomArrayList<T> implements List<T> {
             }
         }
         return false;
+    }
+
+    @Override
+    public void clear() {
+        data = new Object[CAPACITY];
+        size = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) data[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        T prevElement = (T) data[index];
+        data[index] = element;
+        return prevElement;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (size == data.length) {
+            int newLength = (data.length * 3) / 2 + 1;
+            data = Arrays.copyOf(data, newLength);
+        }
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = element;
+    }
+
+    @Override
+    public T remove(int index) {
+        int length = data.length - index;
+        T value = (T) data[index];
+        System.arraycopy(data, index + 1, data, index, length - 1);
+        size--;
+        return value;
     }
 
     @Override
@@ -103,48 +162,6 @@ public class CustomArrayList<T> implements List<T> {
     }
 
     @Override
-    public void clear() {
-        data = new Object[CAPACITY];
-        size = 0;
-    }
-
-    @Override
-    public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        return (T) data[index];
-    }
-
-    @Override
-    public T set(int index, T element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, T element) {
-        if (index < 0 || index >= size){
-            throw new IndexOutOfBoundsException();
-        }
-
-        if (size == data.length) {
-            int newLength = (data.length * 3) / 2 + 1;
-            data = Arrays.copyOf(data, newLength);
-        }
-        System.arraycopy(data,index,data,index+1,size-index);
-        data[index] = element;
-    }
-
-    @Override
-    public T remove(int index) {
-        int length = data.length - index;
-        T value = (T) data[index];
-        System.arraycopy(data, index + 1, data, index, length-1);
-        size--;
-        return value;
-    }
-
-    @Override
     public int indexOf(Object o) {
         return 0;
     }
@@ -156,12 +173,71 @@ public class CustomArrayList<T> implements List<T> {
 
     @Override
     public ListIterator<T> listIterator() {
-        return null;
+        return listIterator(0);
     }
 
     @Override
     public ListIterator<T> listIterator(int index) {
-        return null;
+        return new ListIterator<T>() {
+            int current = index;
+
+            @Override
+            public boolean hasNext() {
+                return current < size;
+            }
+
+            @Override
+            public T next() {
+                if(!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) data[current++];
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return current > 0;
+            }
+
+            @Override
+            public T previous() {
+                if(!hasPrevious()){
+                    throw new NoSuchElementException();
+                }
+                return (T) data[current - 1];
+            }
+
+            @Override
+            public int nextIndex() {
+                if(!hasNext()){
+                    throw new NoSuchElementException();
+                }
+                return current + 1;
+            }
+
+            @Override
+            public int previousIndex() {
+                if(!hasPrevious()){
+                    throw new NoSuchElementException();
+                }
+                return current - 1;
+            }
+
+            @Override
+            public void remove() {
+                CustomArrayList.this.remove(data[current--]);
+            }
+
+            @Override
+            public void set(T element) {
+                data[index] = element;
+            }
+
+            @Override
+            public void add(T element) {
+                CustomArrayList.this.add(current++, element);
+            }
+        };
     }
 
     @Override
